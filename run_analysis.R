85e96158792eb6bbebcad027f3e1ea47cc5d860e
Philip KP <philip.kp@gmail.com>
2015-10-24 04:35:33 +0530

    R script file and the result file being loaded.

---
+++ run_analysis.R
@@ -0,0 +1,63 @@
+##  
+##  Course Id: getdata-033
+##  Author : XXXXXXXXXXXXXXXXXXXXXXXXXXXX (hidden for reason of peer evaluation)
+##  Date: 23 Oct 2015
+##  
+##  Assumptions:
+##  ~~~~~~~~~~~~
+##  
+##  1. Data is downloaded and unzipped into <working dir>/c3projDataset
+##  
+##  
+##  
+##  
+##  
+
+run_analysis <- function(){
+
+  
+  # verify the data set path 
+  setwd("C:/Users/pphilipose/Documents/r_working_dir")
+  if(! dir.exists("./UCI HAR Dataset")) { stop ("Data dir missing. Program terminated")}
+ 
+  
+  # get labels & features 
+  actLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+  features <- read.table("./UCI HAR Dataset/features.txt")
+  
+  # filtering for features on mean and standard deviation only
+  reqFeatures <- grep("-(mean|std)\\(\\)", features[, 2])
+  reqFeaturesNames <- features[reqFeatures,2] # getting names
+  reqFeaturesNames <- gsub('-mean', 'Mean', reqFeaturesNames)
+  reqFeaturesNames <- gsub('-std', 'Std', reqFeaturesNames)
+  reqFeaturesNames <- gsub('[-()]', '', reqFeaturesNames)
+  
+  # load the training datasets
+  dsTraining <- read.table("UCI HAR Dataset/train/X_train.txt")[reqFeatures]
+  dsTrainAct <- read.table("UCI HAR Dataset/train/Y_train.txt")
+  dsTrainSub <- read.table("UCI HAR Dataset/train/subject_train.txt")
+  dsAllTrain <- cbind(dsTrainSub, dsTrainAct, dsTraining)
+  
+  # load the test datasets
+  dsTesting <- read.table("UCI HAR Dataset/test/X_test.txt")[reqFeatures]
+  dsTestAct <- read.table("UCI HAR Dataset/test/Y_test.txt")
+  dsTestSub <- read.table("UCI HAR Dataset/test/subject_test.txt")
+  dsAllTest <- cbind(dsTestSub, dsTestAct, dsTesting)
+  
+  # merging testing and training datasets with labels
+  dsAllData <- rbind(dsAllTrain, dsAllTest)
+  colnames(dsAllData) <- c("Subject", "Activity", reqFeaturesNames)
+  
+  # activities & subjects into factors
+  dsAllData$Activity <- factor(dsAllData$Activity, levels = actLabels[,1], labels = actLabels[,2])
+  dsAllData$Subject <- as.factor(dsAllData$Subject)
+  
+  # melt data
+  library(reshape2)
+  
+  dsAllDataMelted <- melt(dsAllData, id = c("Subject", "Activity"))
+  dsAllDataMean <- dcast(dsAllDataMelted, Subject + Activity ~ variable, mean)
+  
+  write.table(dsAllDataMean, "tidy.txt", row.names = FALSE, quote = FALSE)
+  
+} # end function
